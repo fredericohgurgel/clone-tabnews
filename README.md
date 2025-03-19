9c264ca799598a02f96f58b761b5a764104801f8
fredericohgurgel <160526884+fredericohgurgel@users.noreply.github.com>
2025-03-19 13:22:48 +0000

    adiciona script `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Criação acompanhada do curso.dev do Filipe Deschamps! Projeto aprendendo programação FrontEnd com JavaScript
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-    function Home(){
-        return <h1>Linda, eu te amo! Você é muito importante para mim!</h1>
-    }
+function Home() {
+  return <h1>Linda, eu te amo! Você é muito importante para mim!</h1>;
+}
 
-    export default Home;
+export default Home;
