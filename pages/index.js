8835194ba9f1b8a5fa16ca6a4da1f5ca1adea1d7
fredericohgurgel <160526884+fredericohgurgel@users.noreply.github.com>
2025-03-20 22:50:44 +0000

    Ajustando frase inicial da página em construção!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Linda, eu te amo! Você é muito importante para mim!</h1>;
+  return (
+    <h1>
+      Essa página está em construção e servirá como portfólio de projetos
+      desenvolvidos por Frederico Gurgel!
+    </h1>
+  );
 }
 
 export default Home;
